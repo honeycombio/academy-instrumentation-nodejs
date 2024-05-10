783e9b9973fd32d8305be21a0aeb8762dc049dd6
dlsdlsdls <dianelestrain@gmail.com>
2024-05-10 10:48:05 -0700

    changes?

--- services/meminator-nodejs/src/featureFlags.ts
+++
@@ -1,9 +0,0 @@
-import { trace } from '@opentelemetry/api';
-
-export class FeatureFlags {
-    useLibrary(): boolean {
-        const value = Math.random() < 0.25;
-        trace.getActiveSpan()?.setAttribute('app.featureFlag.useLibrary', value);
-        return value;
-    }
-}
